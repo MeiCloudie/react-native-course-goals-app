d9fbdf32e5283cd7b8b51def4df17bea62d3d798
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-07-12 20:39:25 +0700

    feat: Styling React Native Apps

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
       </View>
-      <Text>Hello World!</Text>
-      <Button title='Tap me!' />
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -15,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "blue",
   },
 });
