fa78453b707fd4c51e225e3d9c0e4ba4644eb7b7
Truong Thuc Van <truongthucvan1242@gmail.com>
2023-07-14 14:33:16 +0700

    feat: Making Items Deletable & Using IDs

--- App.js
+++ App.js
@@ -15,8 +15,11 @@ export default function App() {
     ]);
   }
 
-  function deleteGoalHandler() {
-    console.log("DELETE");
+  function deleteGoalHandler(id) {
+    // console.log("DELETE");
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
@@ -30,6 +33,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
